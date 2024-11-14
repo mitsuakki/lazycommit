67c3074bbde2a8f6ff632f2a0e515772d1d02e24
Alexis Daugé <alexis.dauge2004@gmail.com>
2024-11-14 16:40:47 +0100

    add: cli handling

---
+++ cmd/root.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Short: "AI-powered git hook to generate commit messages from the staged diff.",
+	SilenceUsage: true,
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
