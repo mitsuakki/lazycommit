72525962ec6ad316782a4f03cb9afe0eee7d03c5
Alexis Daugé <alexis.dauge2004@gmail.com>
2024-11-14 16:40:56 +0100

    add(cmd): version command

---
+++ cmd/version.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+type Version struct {
+	Major int
+	Minor int
+	Patch int
+}
+
+type Commit struct {
+	Hash string
+}
+
+var (
+	version Version
+	commit  Commit
+)
+
+var versionCmd = &cobra.Command{
+	Use: "version",
+	Short: "Print the version number of lazycommit",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version: %d.%d.%d\n", version.Major, version.Minor, version.Patch)
+		fmt.Printf("Commit: %s\n", commit.Hash)
+	},
+}
